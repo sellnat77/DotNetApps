0ecf4f1bb6a17bf17fbe9884a473d8572b5a7572
sellnat77 <sell_nat@yahoo.com>
2015-10-04 21:48:11 -0700

    Finished the winforms portion of linq

---
+++ Linq/BooksLinqWinForm/BooksLinqWinForm/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BooksLinqWinForm
+{
+    partial class LinqForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputWindow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // outputWindow
+            // 
+            this.outputWindow.Location = new System.Drawing.Point(12, 12);
+            this.outputWindow.Multiline = true;
+            this.outputWindow.Name = "outputWindow";
+            this.outputWindow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputWindow.Size = new System.Drawing.Size(705, 595);
+            this.outputWindow.TabIndex = 0;
+            // 
+            // LinqForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 619);
+            this.Controls.Add(this.outputWindow);
+            this.Name = "LinqForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputWindow;
+    }
+}
+
---
+++ Linq/BooksLinqWinForm/BooksLinqWinForm/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooksLinqWinForm
+{
+    public partial class LinqForm : Form
+    {
+        public LinqForm()
+        {
+            InitializeComponent();
+        }
+
+        //Initialize the form with the required data
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Get the database context
+            BooksLinqWinForm.BooksEntities bookDBContxt = new BooksLinqWinForm.BooksEntities();
+
+            //Titles and authors query
+            var TitlesAndAuths = from authors in bookDBContxt.Authors
+                       from books in authors.Titles
+                       orderby books.Title1
+                       select new { books.Title1, authors.FirstName, authors.LastName };
+            outputWindow.AppendText("Titles and Authors:\r\n\r\n");
+
+            foreach(var item in TitlesAndAuths)
+            {
+                outputWindow.AppendText(item.Title1 + " " + item.FirstName + " " + item.LastName + "\r\n");
+            }
+
+            //Titles and authors query sorted by title
+            var AuthsAndTitlesSort = from authors in bookDBContxt.Authors
+                       from books in authors.Titles
+                       orderby books.Title1, authors.LastName, authors.FirstName
+                       select new { books.Title1, authors.FirstName, authors.LastName };
+
+            outputWindow.AppendText("\r\n\r\nAuthors and titles with authors sorted for each title:\r\n\r\n");
+            
+            foreach (var item in AuthsAndTitlesSort)
+            {
+                outputWindow.AppendText(item.Title1 + " " + item.FirstName + " " + item.LastName + "\r\n");
+            }
+            
+            //Titles query grouped by authors
+            var TitlesGrouped = from books in bookDBContxt.Titles
+                        orderby books.Title1
+                        select new
+                        {
+                            bookTitle = books.Title1,
+                            //Grouping
+                            authors = from auth in books.Authors
+                                      orderby auth.LastName, auth.FirstName
+                                      select new { auth.FirstName, auth.LastName }
+                        };
+
+            outputWindow.AppendText("\r\n\r\nTitles grouped by author:\r\n\r\n");
+            
+            foreach (var title in TitlesGrouped)
+            {
+                outputWindow.AppendText(title.bookTitle + ":\r\n");
+                foreach(var auth in title.authors)
+                {
+                    outputWindow.AppendText(auth.FirstName + " " + auth.LastName + "\r\n");
+                }
+                outputWindow.AppendText("\r\n");
+            }
+        }
+    }
+}
