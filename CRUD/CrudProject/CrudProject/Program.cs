1bbb1f6f4b8597e2c5806918d49b882476ed6673
sellnat77 <sell_nat@yahoo.com>
2015-10-15 18:32:14 -0700

    Starting crud project

---
+++ CRUD/CrudProject/CrudProject/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            readStudents();
+
+        }
+
+        static void readStudents()
+        {
+            SchoolDBEntities school = new SchoolDBEntities();
+            var students = from st in school.Students 
+                            select st;
+            foreach(var st in students)
+            {
+                Console.WriteLine(st.StudentName);
+            }
+        }
+
+        static void updateStudents()
+        {
+
+        }
+
+        static void deleteStudents()
+        {
+
+        }
+
+        static void createStandard()
+        {
+
+        }
+
+        static void readStandard()
+        {
+
+        }
+
+        static void updateStandard()
+        {
+
+        }
+
+        static void deleteStandard()
+        {
+
+        }
+    }
+}
