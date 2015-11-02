d3ab9fc4fbdd3b2a9ed7b15e217f1922043824d6
sellnat77 <sell_nat@yahoo.com>
2015-11-01 21:34:04 -0800

    Finisehd part 2.1

---
+++ xml/LINQtoXML-cs/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LINQtoXML_cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XDocument xmlDoc = XDocument.Load("People.xml");
+
+            var persons = from person in xmlDoc.Descendants("Person")
+            select new
+            {
+                Name = person.Element("Name").Value,
+                Address = person.Element("Address") != null ? person.Element("Address").Value : string.Empty,
+                City = person.Element("City").Value,
+                Age = person.Element("Age").Value,
+            };
+
+            txtResults.Text = "";
+            foreach (var person in persons)
+            {
+                txtResults.Text = txtResults.Text + "Name: " + person.Name + "\n";
+                txtResults.Text = txtResults.Text + "Address: " + person.Address + "\n";
+                txtResults.Text = txtResults.Text + "City: " + person.City + "\n";
+                txtResults.Text = txtResults.Text + "Age: " + person.Age + "\n\n";
+            }
+
+            if (txtResults.Text == "")
+                txtResults.Text = "No Results.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XDocument xmlDoc = XDocument.Load("People.xml");
+            
+            xmlDoc.Element("Persons").Add(new XElement("Person", new XElement("Name", txtName.Text), 
+                new XElement("Address", txtAddress.Text), new XElement("City", txtCity.Text), new XElement("Age", txtAge.Text)));
+
+            xmlDoc.Save("People.xml");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
