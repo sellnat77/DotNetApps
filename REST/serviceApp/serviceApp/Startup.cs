eec4a703a77c42ef12f77944cd21472a7acf26d6
sellnat77 <sell_nat@yahoo.com>
2015-11-08 21:51:31 -0800

    Started proper lab 7

---
+++ REST/serviceApp/serviceApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(serviceApp.Startup))]
+namespace serviceApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
